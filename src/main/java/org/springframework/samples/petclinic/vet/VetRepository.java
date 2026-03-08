ccd0c0b4733cb6bfae4a5d0af7072419232fad51
Yashodha Fernando <118588673+Yashoda-Fr@users.noreply.github.com>
2026-03-08 13:58:38 +0530

    Update VetRepository.java
    Signed-off-by: Yashodha Fernando <118588673+Yashoda-Fr@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
@@ -10,17 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
-/**
- * Repository class for <code>Vet</code> domain objects All method names are compliant
- * with Spring Data naming conventions so this interface can easily be extended for Spring
- * Data. See:
- * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
- *
- * @author Ken Krebs
- * @author Juergen Hoeller
- * @author Sam Brannen
- * @author Michael Isvy
- */
 public interface VetRepository extends Repository<Vet, Integer> {
 
 	/**
